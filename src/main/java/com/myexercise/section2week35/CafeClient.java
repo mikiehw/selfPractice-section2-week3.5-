3666220e65656197e48584be2a58b2ed0805b143
mikiehw <hwchokim95@hanmail.net>
2023-02-05 15:45:23 +0900

    DI 적용 후, 생성자 주입한 코드

--- src/main/java/com/myexercise/section2week35/CafeClient.java
+++ src/main/java/com/myexercise/section2week35/CafeClient.java
@@ -3,6 +3,7 @@ package com.myexercise.section2week35;
 import java.util.List;
 
 public class CafeClient {
-    MenuController menuController = new MenuController();
+    MenuService menuService = new MenuService();  // 1. 객체를 생성해서,
+    MenuController menuController = new MenuController(menuService); // 2. 객체를 주입함.*의존성 주입
     List<String> menuList = menuController.getMenuList();
 }
--- src/main/java/com/myexercise/section2week35/MenuController.java
+++ src/main/java/com/myexercise/section2week35/MenuController.java
@@ -3,7 +3,10 @@ package com.myexercise.section2week35;
 import java.util.List;
 
 public class MenuController {
-    MenuService menuService = new MenuService();
+    MenuService menuService;
+    public MenuController(MenuService menuService) { // 객체를 주입 받기 위한 생성자 작성.
+        this.menuService = menuService;
+    }
     public List<String> getMenuList() {
         return menuService.getMenuList();
     }
